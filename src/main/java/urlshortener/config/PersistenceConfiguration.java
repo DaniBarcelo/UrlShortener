0d9e03f29048b2372cb22fdbf6d447175a52f283
Alejandro Omist <alejandro.omist.98@hotmail.com>
2020-11-12 21:14:26 +0100

    information added

--- src/main/java/urlshortener/config/PersistenceConfiguration.java
+++ src/main/java/urlshortener/config/PersistenceConfiguration.java
@@ -3,10 +3,15 @@ package urlshortener.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import urlshortener.domain.SystemInfo;
 import urlshortener.repository.ClickRepository;
 import urlshortener.repository.ShortURLRepository;
 import urlshortener.repository.impl.ClickRepositoryImpl;
+import urlshortener.repository.SystemInfoRepository;
 import urlshortener.repository.impl.ShortURLRepositoryImpl;
+import urlshortener.repository.impl.SystemInfoRepositoryImpl;
+
 
 @Configuration
 public class PersistenceConfiguration {
